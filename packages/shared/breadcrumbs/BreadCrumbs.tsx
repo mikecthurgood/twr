e085cd3e71351a7d25052ce650c3a3a840b5ecb7
Mike Thurgood <mike.thurgood@oddschecker.com>
2022-01-06 12:05:14 +0000

    feat(bread crumbs): create bread crumbs component

---
+++ packages/shared/breadcrumbs/BreadCrumbs.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BreadcrumbsContainer } from './style';
+
+export const Breadcrumbs = (): JSX.Element => {
+  return (
+    <BreadcrumbsContainer>
+      <p>Footer Content goes here</p>
+    </BreadcrumbsContainer>
+  );
+};
---
+++ packages/shared/breadcrumbs/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const BreadcrumbsContainer = styled.div`
+  width: 100%;
+  height: 256px;
+  clear: both;
+  background: var(--twr-primary-white);
+  margin-top: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 3px solid var(--twr-primary-gray);
+  p {
+    font-family: var(--twr-font-heading);
+  }
+`;
