c4d88763ba4ca72e7d35a09c685a70dcf2be7f2d
Mike Thurgood <mike.thurgood@oddschecker.com>
2022-01-06 11:53:11 +0000

    feat(footer): create footer component

---
+++ packages/shared/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FooterContainer } from "./style";
+
+export const Footer = (): JSX.Element => {
+	return (
+		<FooterContainer>
+			<p>Footer Content goes here</p>
+		</FooterContainer>
+	);
+};
